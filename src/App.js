3e62b0f1d35c7b8fbf8acf83be660911f7ae81ba
fallmustapha121@gmail.com <Fatimata12111997>
2022-08-02 17:27:02 +0100

    Component state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import Button from "./components/button/component";
 import TaskList from "./components/task-list/component";
 import Task from "./components/task/component";
 import TextBox from "./components/text-box/component";
 
+const lst = [
+  {title:"Manger"},
+  {title: "Dormir"},
+  {title: "Shopping"}
+]
+
 function App() {
+  const [list,setList] = useState(lst);
+  const [newTask, setNewTask] = useState("")
+  const onTextChange=(evt)=>{
+    setNewTask(evt.target.value)
+    console.log(newTask)
+  }
+  const onButtonClicked=(evt)=>{
+    setList(lastList=>[...lastList,{title:newTask}]);
+  }
   return (
     <>
       <div style={{display:'flex', margin:'auto',marginTop:'30px', width:'50%',flexDirection:'row', justifyContent:'space-between' }}>
-        <TextBox/>
-        <Button/>
+        <TextBox onValueChange={onTextChange}/>
+        <Button onClick={onButtonClicked}/>
       </div>
       <div>
-        <TaskList/>
+        <TaskList list={list}/>
       </div>
     </>
     
--- src/components/task-list/component.js
+++ src/components/task-list/component.js
@@ -2,10 +2,12 @@ import Task from '../task/component'
 import './task-list.css'
 
 
-export default function TaskList(){
+export default function TaskList({list}){
     return (
         <div className='task_list-container'>
-            <Task/>
+            {
+                list.map(task=><Task title={task.title}></Task>)
+            }
         </div>
     )
 }
